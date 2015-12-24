ae375ba6454e7a2057f9935e8fc374d565e9a6b8
Christophe Dumont <christophedumont@MacBook-Pro-de-Christophe.local>
2015-12-24 15:40:33 +0100

    Added events exemples & simple read server

---
+++ events/3.readfile.js
@@ -0,0 +1,10 @@
+var fs = require("fs");
+
+fs.readFile('input.txt', function (err, data) {
+   if (err){
+      console.log(err.stack);
+      return;
+   }
+   console.log(data.toString());
+});
+console.log("Program Ended");
